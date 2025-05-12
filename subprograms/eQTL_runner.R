5a389051bc6711c0885781908b27c07fb0734a0b
Luis Javier Madrigal-Roca <108612426+luismadrigal98@users.noreply.github.com>
2025-05-12 10:18:24 -0500

    Add 'qtl2' package to required packages and import genotype and phenotype data

--- subprograms/eQTL_runner.R
+++ subprograms/eQTL_runner.R
@@ -37,7 +37,7 @@ for(file in list.files(path = R_src_dir, full.names = TRUE, pattern = "\\.R$"))
   rm(file)
 }
 
-required_packages <- c('qtl', 'dplyr')
+required_packages <- c('qtl', 'qtl2', 'dplyr')
 
 set_environment(parallel_backend = T, personal_seed = 1998, 
                 automatic_download = T, required_pckgs = required_packages)
@@ -45,3 +45,13 @@ set_environment(parallel_backend = T, personal_seed = 1998,
 # ******************************************************************************
 # 2) Load the data ----
 # ______________________________________________________________________________
+
+# 2.1) Import the genotype data ----
+geno_data <- read.table(file = '/home/l338m483/scratch/MG_SF_SWB_project/R_directory/eQTL_Data/62.rQTL.genotype.txt',
+                        header = T, sep = '\t', stringsAsFactors = F)
+
+# 2.2) Import the phenotype data ----
+# Expression profiles per gene per sample
+
+pheno_data <- read.table(file = '/home/l338m483/scratch/MG_SF_SWB_project/R_directory/eQTL_Data/gene.group1_62.txt',
+                          header = T, sep = '\t', stringsAsFactors = F)
